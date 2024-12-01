f4c02e340f45a7bafe4f78cd26d8fc31fe2eaa4b
ALADETAN-IFE <famousfotune123@gmail.com>
2024-12-01 18:40:54 +0100

    did signup and signin api consumption

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,44 +1,91 @@
-import React from 'react'
-import "../Style/Login.css"
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import "../Style/Login.css";
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Swal from 'sweetalert2';
+import { PacmanLoader } from 'react-spinners';
 
 const Login = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [email, setemail] = useState();
+  const [password, setpassword] = useState();
+  const [error, seterror] = useState(null);
+  const [loading, setloading] = useState(false);
+
+
+  const handleSignIn = async () => {
+    const data = { email, password }
+    try {
+      setloading(true)
+      const response = await axios.post("https://taskmaserver.onrender.com/api/v1/user/signin", data)
+      console.log(response)
+      Swal.fire({
+        title: 'Successful!',
+        text: response?.data?.message,
+        icon: 'success',
+      });
+      setTimeout(() => {
+        // navigate("/home")
+      }, 2000);
+    } catch (error) {
+      console.error(error)
+      // seterror(error?.data?.message)
+      console.log(error?.data?.message)
+    } finally {
+      setloading(false)
+    }
+  }
   return (
     <div className='Login'>
-         <div className='Welcome-header'>
-        <button className='Welcome-header-icon' onClick={(()=>navigate('/welcome'))}></button>
+      <div className='Welcome-header'>
+        <button className='Welcome-header-icon' onClick={(() => navigate('/welcome'))}></button>
         <div className='Welcome-header-logo'></div>
       </div>
       <div className='Login-container'>
         <div className='login-header'>Login</div>
-        
+
       </div>
       <div className='Login-Input-Holder'>
-            <div className='login-input-container'>
-                <div className='input-icon'>
-                <img src="../../public/mage_email.png" alt="" />
+        <div className='login-input-container'>
+          <div className='input-icon'>
+            <img src="../../public/mage_email.png" alt="" />
 
-                </div>
-                <input type="email"  placeholder='Email Address' className='Email-input'/>
+          </div>
+          <input type="email" onChange={(e) => setemail(e.target.value)} placeholder='Email Address' className='Email-input' />
+        </div>
+        <div className='login-input-container'>
+          <div className='input-icon'>
+            <img src="../../public/Vector.png" alt="Vector" />
+          </div>
+          <input type="password" onChange={(e) => setpassword(e.target.value)} placeholder='Password' className='Password-input' />
+          <div className='input-iconp'>
+            <img src="../../public/Vector1.png" alt="Vector" />
+          </div>
+        </div>
+        {
+          error ?
+            <div style={{
+              color: "#e71e1e", background: "#d2cccc",
+              marginBottom: "8px", padding: "5px", fontFamily: "Poppins", width: "max-content"
+            }}>
+
+              {error}
             </div>
-            <div className='login-input-container'>
-                <div className='input-icon'>
-                    <img src="../../public/Vector.png" alt="Vector" />
-                </div>
-                <input type="password"  placeholder='Password'  className='Password-input'/>
-                <div className='input-iconp'>
-                    <img src="../../public/Vector1.png" alt="Vector" />
-                </div>
-            </div>
-            </div>
-            <div className='Login-form'>
-              <div className='forms'> 
-                <p className='forgot' onClick={(()=>navigate('/reset-password'))}>Forget password?</p>
-              </div>
-              <button className='Login-Btn'onClick={(()=>navigate("/home"))}>Login</button>
-            </div>
-            <div className='Login-footer'>Don’t have an account? <span onClick={(()=>navigate("/sign-up"))}>Sign Up</span></div>
+            : null
+        }
+      </div>
+      <div className='Login-form'>
+        <div className='forms'>
+          <p className='forgot' onClick={(() => navigate('/reset-password'))}>Forget password?</p>
+        </div>
+        <button className='Login-Btn' onClick={handleSignIn}>
+          {
+            loading ?
+              <PacmanLoader color="#ece6e6" size={17} />
+              : "Login"
+          }</button>
+      </div>
+      <div className='Login-footer'>Don’t have an account? <span onClick={(() => navigate("/sign-up"))}>Sign Up</span></div>
 
     </div>
   )
--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,55 +1,114 @@
-import React from 'react'
-import "../Style/SignUp.css"
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import "../Style/SignUp.css";
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { PacmanLoader } from 'react-spinners';
+import Swal from 'sweetalert2';
 
 const SignUp = () => {
-    const navigate = useNavigate()
-    const [email, setemail] = useState();
-    const [activeItem, setActiveItem] = useState(0);
-    const handleSignUp = () => {
+  const navigate = useNavigate()
+  const [email, setemail] = useState();
+  const [password, setpassword] = useState();
+  const [confirmPassword, setconfirmPassword] = useState();
+  const [error, seterror] = useState(null);
+  const [loading, setloading] = useState(false);
 
+  useEffect(() => {
+    if (password == confirmPassword) {
+      seterror(null)
+    } else {
+      seterror("Passwords do not match")
     }
+  }, [confirmPassword])
+
+
+  const handleSignUp = async () => {
+    if (error) {
+      seterror("Passwords do not match")
+    } else {
+      const data = { email, password }
+      try {
+        setloading(true)
+        const response = await axios.post("https://taskmaserver.onrender.com/api/v1/user/signup", data)
+        console.log(response)
+        Swal.fire({
+          title: 'Successful!',
+          text: response?.data?.message,
+          icon: 'success',
+        });
+        setTimeout(() => {
+          // navigate('/sign-in')
+        }, 2000);
+      } catch (error) {
+        console.error(error)
+      seterror(error?.response?.data?.message)
+      setTimeout(() => {
+        seterror(null)
+        // navigate('/sign-in')
+      }, 10000);
+      // console.log(error?.response?.data?.message)
+      } finally {
+        setloading(false)
+      }
+    }
+  }
   return (
     <div className='SignUp'>
-         <div className='Welcome-header'>
-        <button className='Welcome-header-icon' onClick={(()=>navigate('/welcome'))}></button>
+      <div className='Welcome-header'>
+        <button className='Welcome-header-icon' onClick={(() => navigate('/welcome'))}></button>
         <div className='Welcome-header-logo'></div>
       </div>
       <div className='SignUp-main'>
         <div className='CreateAccount-text-holder'>Create Acccount</div>
         <div className='Input-Holder'>
-            <div className='input-container'>
-                <div className='input-icon'>
-                <img src="../../public/mage_email.png" alt="" />
+          <div className='input-container'>
+            <div className='input-icon'>
+              <img src="../../public/mage_email.png" alt="" />
 
-                </div>
-                <input type="email" onChange={(e)=> setemail(e.target.value)} placeholder='Email Address' className='Email-input'/>
             </div>
-            <div className='input-container'>
-                <div className='input-icon'>
-                    <img src="../../public/Vector.png" alt="Vector" />
-                </div>
-                <input type="password"  placeholder='Password'  className='Password-input'/>
-                <div className='input-iconp'>
-                    <img src="../../public/Vector1.png" alt="Vector" />
-                </div>
+            <input type="email" onChange={(e) => setemail(e.target.value)} placeholder='Email Address' className='Email-input' />
+          </div>
+          <div className='input-container'>
+            <div className='input-icon'>
+              <img src="../../public/Vector.png" alt="Vector" />
             </div>
-            <div className='input-container'>
-                <div className='input-icon'>
-                    <img src="../../public/Vector.png" alt="Vector" />
-                </div>
-                <input type="password"  placeholder=' Confirm Password'  className='Password-input'/>
-                <div className='input-iconp'>
-                    <img src="../../public/Vector1.png" alt="Vector" />
-                </div>
+            <input type="password" onChange={(e) => setpassword(e.target.value)} placeholder='Password' className='Password-input' />
+            <div className='input-iconp'>
+              <img src="../../public/Vector1.png" alt="Vector" />
             </div>
+          </div>
+          <div className='input-container'>
+            <div className='input-icon'>
+              <img src="../../public/Vector.png" alt="Vector" />
+            </div>
+            <input type="password" onChange={(e) => setconfirmPassword(e.target.value)} placeholder=' Confirm Password' className='Password-input' />
+            <div className='input-iconp'>
+              <img src="../../public/Vector1.png" alt="Vector" />
+            </div>
+          </div>
+          {
+            error ?
+              <div style={{
+                color: "#e71e1e", background: "#d2cccc",
+                marginBottom: "8px", padding: "5px", fontFamily: "Poppins", width: "max-content"
+              }}>
+
+                {error}
+              </div>
+              : null
+          }
         </div>
       </div>
       <div className='button-container-signup'>
 
-        <button className='create-account-btn'>Sign Up</button>
+        <button className='create-account-btn' onClick={handleSignUp}>
+          {
+            loading ?
+              <PacmanLoader color="#ece6e6" size={17} />
+              : "Sign Up"
+          }</button>
       </div>
-        <div className='Form'>already have account? <span onClick={(()=>navigate('/sign-in'))}>Login</span></div>
+      <div className='Form'>already have account? <span onClick={()=> navigate('/sign-in')}>Login</span></div>
     </div>
   )
 }
